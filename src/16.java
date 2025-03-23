34547bc62b6b96182497572efc2ef4dea0fa377a
birkenheadbellmay25 <birkenheadbellmay25@gmail.com>
2025-03-23 17:32:45 +0000

    Add 16.java

---
+++ src/16.java
@@ -0,0 +1,8 @@
+public class SportsStats {
+    public void trackSports() {
+        // Example of tracking sports data
+        System.out.println("Athlete 1: Score: " + 50);
+        System.out.println("Athlete 2: Score: " + 78);
+        System.out.println("Athlete 3: Score: " + 64);
+    }
+}
