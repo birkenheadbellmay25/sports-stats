8174f2cb638ae91e23870b197f3f1968bd235ee5
birkenheadbellmay25 <birkenheadbellmay25@gmail.com>
2025-04-03 23:10:14 +0000

    Add 23.java

---
+++ src/23.java
@@ -0,0 +1,9 @@
+public class SportsStats {
+    public static void main(String[] args) {
+        // Example of tracking sports stats
+        System.out.println("Track 1: Running");
+        System.out.println("Track 2: Cycling");
+        System.out.println("Track 3: Swimming");
+        System.out.println("Track 4: Football");
+    }
+}
