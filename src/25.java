fbb6473103f5fa35209af0dbb3dfa457711213f6
birkenheadbellmay25 <birkenheadbellmay25@gmail.com>
2025-04-06 03:55:26 +0000

    Add 25.java

---
+++ src/25.java
@@ -0,0 +1,8 @@
+public class SportsStats {
+    public static void main(String[] args) {
+        // Add your sports statistics analysis code here
+        // Example:
+        System.out.println("Average player age: " + (int)(Math.random() * 25));
+        System.out.println("Most active player: " + Math.max(1, 2));
+    }
+}
