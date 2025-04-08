25d8257d9cc96a8f01bac7fcbc9760c0e1159bab
birkenheadbellmay25 <birkenheadbellmay25@gmail.com>
2025-04-08 05:23:02 +0000

    Add 27.java

---
+++ src/27.java
@@ -0,0 +1,17 @@
+public class SportsStats {
+    private int playerId;
+    private String playerName;
+
+    public SportsStats() {
+        // Constructor code here
+    }
+
+    // Add more methods as needed
+
+    public static void main(String[] args) {
+        // Main method to test the sports_stats class
+        SportsStats stats = new SportsStats();
+        System.out.println(stats.playerId);
+        System.out.println(stats.playerName);
+    }
+}
