36a3066846455128c062611d8baefdee16a007be
birkenheadbellmay25 <birkenheadbellmay25@gmail.com>
2025-05-26 22:50:01 +0000

    Add 39.java

---
+++ src/39.java
@@ -0,0 +1,16 @@
+public class SportsStats {
+    public static void main(String[] args) {
+        // Example usage: Print out some numbers from a sports dataset
+        System.out.println("The number of games played in this season was: " + GamesPlayed);
+        
+        // Another example: Calculate the average score per game
+        double averageScore = calculateAverageGameScores();
+        System.out.println("The average game score is: " + averageScore);
+    }
+    
+    private static double calculateAverageGameScores() {
+        // Implementation of calculating average game scores
+        
+        return 0.0;
+    }
+}
